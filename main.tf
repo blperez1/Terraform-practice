71d76894c6b5b38298f26366f10a2eb1cda98062
Unknown <blossomperez18@gmail.com>
2023-05-02 23:20:56 -0500

    Create route table

--- main.tf
+++ main.tf
@@ -36,3 +36,21 @@ resource "aws_internet_gateway" "ig" {
     Name = "ig"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_1_rt" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id 
+}

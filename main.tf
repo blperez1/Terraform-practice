731967daf974276f88416cfb32d48672f44fbe4f
Unknown <blossomperez18@gmail.com>
2023-05-02 23:21:16 -0500

    Create security group

--- main.tf
+++ main.tf
@@ -54,3 +54,37 @@ resource "aws_route_table_association" "public_1_rt" {
   subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id 
 }
+
+resource "aws_security_group" "web_sg" {
+  name   = "HTTP and SSH"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "web_instance" {
+  ami           = "ami-02396cdd13e9a1257"
+  instance_type = var.ec2_instance_type
+
+  tags = {
+    name = var.instance_name
+  }
+}

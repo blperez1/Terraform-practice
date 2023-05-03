fcea2fe8723bdaf0056508c44c5a82c92d44b55c
Unknown <blossomperez18@gmail.com>
2023-05-02 22:31:07 -0500

    Create public and private subnets

--- main.tf
+++ main.tf
@@ -11,7 +11,28 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "0.0.0.0/0"
+
   tags = {
-    name = var.instance_name
+    Name = "Public"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Private"
+  }
+}
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-02396cdd13e9a1257"
+#   instance_type = var.ec2_instance_type
+
+#   tags = {
+#     name = var.instance_name
+#   }
+# }
